633d10f501011a84b51a65ebc095f9401766588e
Anshu Kumari <anshukumari904@gmail.com>
2022-08-22 23:27:10 +0530

    Handling routes using express.js

--- SecondProgram/app.js
+++ SecondProgram/app.js
@@ -3,11 +3,15 @@
 const express= require('express');
 
 const app = express();
-app.use((req,res,next)=>{
-    console.log("In a middleware");
-    next(); // Allows the request to continue to the next middleware in line otherwise request will get die 
+app.use('/',(req,res,next)=>{
+    console.log("Always run");
+    next();
 });
-app.use((req,res,next)=>{
+app.use('/add-product',(req,res,next)=>{
+    console.log("In a middleware");
+    res.send('<h1>Hello from "Add product page".</h1>');
+});
+app.use('/',(req,res,next)=>{
     console.log("In another middleware");
     res.send('<h1>Hello from Express.</h1>');
 });

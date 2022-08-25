a1c6c35b6a8abf30058530fd2ff28fb7afb52187
Anshu Kumari <anshukumari904@gmail.com>
2022-08-25 18:58:30 +0530

    Added express route to add route files

--- SecondProgram/app.js
+++ SecondProgram/app.js
@@ -1,5 +1,3 @@
-//const http = require('http');
-
 const express= require('express');
 const  bodyParser=require('body-parser');
 const app = express();
@@ -10,7 +8,7 @@ app.use('/add-product',(req,res,next)=>{
     console.log("In a middleware");
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
-app.use('/product',(req,res,next)=>{
+app.post('/product',(req,res,next)=>{
     console.log(req.body);
     res.redirect('/');
 });
@@ -18,6 +16,4 @@ app.use('/',(req,res,next)=>{
     console.log("In another middleware");
     res.send('<h1>Hello from Express.</h1>');
 });
-//const server = http.createServer(app);
-
 app.listen(3000);

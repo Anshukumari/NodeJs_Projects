fe6b865bf1be211fffbec79f5f12d1932bb40f56
Anshu Kumari <anshukumari904@gmail.com>
2022-08-28 21:55:58 +0530

    Reading file using fs core module

--- ExpressJs/app.js
+++ ExpressJs/app.js
@@ -10,4 +10,22 @@ app.listen(3000);*/
 const fs = require("fs");
 const fileName = "target.txt";
 
-fs.watch(fileName,()=>console.log('File Changed'));
+//fs.watch(fileName,()=>console.log('File Changed'));
+
+const errHandler = err => console.log(err);
+const dataHandler = data => console.log(data.toString());
+
+//Asynchronous call
+fs.readFile(fileName,(err,data)=>{
+    if(err) errHandler(err)
+    dataHandler(data)
+});
+
+console.log("Node Js  async programming!")
+
+//Synchronous call
+/*const data = fs.readFileSync(fileName);
+console.log(data.toString());
+
+console.log("Node Js  async programming!");*/
+

b5215b769d85fc82883cc9758a0138edfa4bf7b2
Anshu Kumari <anshukumari904@gmail.com>
2022-09-02 15:43:10 +0530

    Added code to connect with MongoDB

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,15 +1,50 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
 //const postRoutes = require("./routes/post");
 
 // by object deStructuring
 
-const {getPosts} = require("./routes/post");
+const postRoutes = require("./routes/post");
 
-app.get("/",getPosts);
+const MONGO_URI = 'mongodb://localhost:27017/NodeJSDB';
+mongoose.connect(
+    MONGO_URI,
+    {useNewUrlParser: true}
+  ).then(()=>console.log('DB Conected'));
 
-const port = 8080;
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`)
+  });
+
+// middleware
+app.use(morgan("dev"));
+
+app.use("/",postRoutes);
+
+const port = 8082;
 
 app.listen(port,()=>{
     console.log(`A NodeJS API is listening on port: ${port}`);
-});
+}); 
+
+/*// import mongoose
+    const mongoose = require('mongoose');
+    // load env variables
+const dotenv = require('dotenv');
+dotenv.config()
+ 
+//db connection
+mongoose.connect(
+  process.env.MONGO_URI,
+  {useNewUrlParser: true}
+)
+.then(() => console.log('DB Connected'))
+ 
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`)
+});*/
+

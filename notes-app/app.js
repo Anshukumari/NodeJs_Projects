faa67844a0b94b4e6091f000fe32c78505ddf2a9
Anshu Kumari <anshukumari904@gmail.com>
2022-09-15 22:10:10 +0530

    Reading arguments from command line
    Reading arguments from command line

--- notes-app/app.js
+++ notes-app/app.js
@@ -22,3 +22,16 @@ console.log(validator.isURL('http://Examplegmail.com'));
 // importing chalk libraray
 const chalk = require('chalk');
 console.log(chalk.green.inverse.bold('Success!'));
+
+// Getting input from Command Arguments
+console.log(process.argv);
+
+const command= process.argv[2];
+if(command == 'add')
+{
+    console.log('Adding Note');
+}
+else if(command=='remove')
+{
+    console.log('Removing Note');
+}
